2069f4f436d836ec1539198a6c1204b1990655b3
clorkapunk <temir225531@gmail.com>
2024-05-05 22:51:22 +0500

    Commit

--- src/screens/tabs/Home.jsx
+++ src/screens/tabs/Home.jsx
@@ -49,30 +49,6 @@ const Home = () => {
         });
     }, []);
 
-    const imgUrls = [
-        "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
-        "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-        "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg",
-        "https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340",
-    ];
-
-    function getRandomData() {
-        let data = [];
-        for (let i = 0; i < 20; i++) {
-            data.push({
-                id: i,
-                owner: `User ${i}`,
-                likes: Math.round(Math.random() * 100),
-                comments: Math.round(Math.random() * 30),
-                picture: imgUrls[Math.floor(Math.random() * imgUrls.length)],
-                description: "My amazing description to this post",
-                title: "My amazing title to this post",
-                published_at: "2024-04-30T10:47:53.950693Z",
-            });
-        }
-        return data;
-    }
-
     const onRefresh = React.useCallback(() => {
         setRefreshing(true);
         setIsNetworkError(false);
