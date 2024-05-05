8c2f7abe31a1812d0ad934d4902054cada21da2e
clorkapunk <temir225531@gmail.com>
2024-05-05 22:53:38 +0500

    Added bottom sheet, gallery from memory, image cropper

--- src/screens/tabs/post/PostCreate.jsx
+++ src/screens/tabs/post/PostCreate.jsx
@@ -1,29 +1,261 @@
-import React, {useEffect} from 'react';
-import {Image, Text, TouchableOpacity, View} from "react-native";
+import React, {useEffect, useRef, useState} from 'react';
+import {Image, ScrollView, ToastAndroid, TouchableOpacity, View} from "react-native";
 import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
-import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import {faArrowLeft, faCrop} from "@fortawesome/free-solid-svg-icons";
 import {useNavigation} from "@react-navigation/native";
-import {Button} from "@rneui/themed";
+import {Button, Input} from "@rneui/themed";
+import ImagePicker from 'react-native-image-crop-picker';
+import {COLORS} from "../../../consts/colors";
+import RNFS, {readFile} from 'react-native-fs'
+import PostPreviewSheet from "../../../components/PostPreviewSheet";
+import {sendPost} from "../../../api/ContentAPI";
+
 
 const PostCreate = ({route}) => {
     const navigation = useNavigation();
-    const {item} = route.params
+    const {item, id} = route.params
+    const [aspectRatio, setAspectRatio] = useState(1)
+    const [imageUri, setImageUri] = useState(item.node.image.uri)
+    const [form, setForm] = useState({
+        title: "",
+        description: "",
+    });
+
+    const [errors, setErrors] = useState({
+        title: "",
+        description: "",
+    });
+    const [postData, setPostData] = useState({
+        picture: imageUri,
+        title: '',
+        description: '',
+        owner: id,
+        published_at: new Date(),
+    })
+
+    const previewSheetRef = useRef(null);
+
+    function onFormChange(name, value) {
+        setForm({...form, [name]: value});
+    }
+
+    function validateForm() {
+        let isValid = true;
+        let errorsTemp = {
+            title: "",
+            description: "",
+        };
+        if (form.title === '') {
+            errorsTemp["title"] = "Enter something";
+            isValid = false;
+        } else {
+            errorsTemp["email"] = "";
+        }
+
+        if (form.description === '') {
+            errorsTemp["description"] = "Enter something";
+            isValid = false;
+        } else {
+            errorsTemp["password"] = "";
+        }
+
+        setErrors(errorsTemp);
+
+        return isValid;
+    }
+
+
+    useEffect(() => {
+        Image.getSize(item.node.image.uri, (width, height) => {
+            setAspectRatio(width / height)
+        })
+
+    }, [item])
+
+    function onShowPreview() {
+        if (!validateForm()) return
+
+        setPostData(prevState => {
+            return {
+                ...prevState,
+                picture: imageUri,
+                title: form.title,
+                description: form.description,
+                published_at: new Date(),
+            }
+        })
+        previewSheetRef.current?.open()
+    }
+
+
+    function onSendPost() {
+        if (!validateForm()) return
+
+        const formData = new FormData()
+        formData.append('title', form.title)
+        formData.append('description', form.description)
+        formData.append('owner', postData.owner)
+        formData.append("picture", {
+            uri:imageUri,
+            name:'userProfile.jpg',
+            type:'image/jpg'
+        })
+
+        sendPost(formData)
+            .then(r => {
+                if (r === 201) {
+                    ToastAndroid.show("Success", ToastAndroid.SHORT)
+                    navigation.goBack()
+                }
+            })
+            .catch(e => {
+                ToastAndroid.show("Error", ToastAndroid.SHORT)
+            })
+
+    }
+
 
     return (
-        <View className='px-4 pt-10'>
-            <View className=''>
-                <TouchableOpacity className="mb-7" onPress={() => navigation.navigate("post")}>
-                    <View>
-                        <FontAwesomeIcon defaultProps={{}} icon={faArrowLeft} color={"black"} size={20}/>
+        <>
+            <ScrollView className='px-4 pt-5'>
+                <View className='flex-row justify-between items-center mb-3'>
+                    <TouchableOpacity
+                        className="p-2" onPress={() => navigation.navigate("post")}>
+                        <View className='items-center'>
+                            <FontAwesomeIcon icon={faArrowLeft} color={"black"} size={25}/>
+                        </View>
+                    </TouchableOpacity>
+                    <Button
+                        onPress={() => {
+                            ImagePicker.openCropper({
+                                path: item.node.image.uri,
+                                freeStyleCropEnabled: true
+                            }).then(image => {
+                                setImageUri(image.path)
+                                setAspectRatio(image.width / image.height)
+                            });
+                        }}
+                        buttonStyle={{
+                            backgroundColor: 'white',
+                            borderRadius: 10,
+                            alignItems: 'center',
+                            borderWidth: 2,
+                            borderColor: 'black'
+                        }}
+                        titleStyle={{
+                            fontSize: 16,
+                            color: 'black'
+                        }}
+                    >
+                        <FontAwesomeIcon
+                            style={{marginRight: 10}}
+                            size={20}
+                            icon={faCrop}
+                            color={'black'}
+                        />
+                        Edit image
+                    </Button>
+                </View>
+                <View className='mb-10'>
+                    <View className='mb-5 border-2 rounded border-black'>
+                        <Image
+                            style={{aspectRatio: aspectRatio, width: '100%'}}
+                            source={{uri: imageUri}}
+                        />
                     </View>
-                </TouchableOpacity>
-                <Image
-                    style={{width: '100%', aspectRatio: 1}}
-                    source={{uri: item.node.image.uri}}
-                />
 
-            </View>
-        </View>
+
+                    <Input
+                        onChangeText={(val) => onFormChange("title", val)}
+                        value={form.title}
+                        placeholder={"Write description of your image"}
+                        label={'Title'}
+                        errorMessage={errors.title}
+                        containerStyle={{paddingHorizontal: 0}}
+                        inputContainerStyle={{
+                            paddingHorizontal: 5, borderRadius: 5,
+                            borderWidth: 1, borderColor: 'black'
+                        }}
+                        inputStyle={{color: "black"}}
+                        labelStyle={{
+                            fontSize: 20,
+                            color: "black",
+                            marginBottom: 5,
+                            fontWeight: "100"
+                        }}
+                        placeholderTextColor={COLORS.lightGrey}
+                        errorStyle={{color: "crimson"}}
+                    />
+
+                    <Input
+                        multiline={true}
+                        onChangeText={(val) => onFormChange("description", val)}
+                        value={form.description}
+                        placeholder={"Write description of your image"}
+                        label={'Description'}
+                        errorMessage={errors.description}
+                        numberOfLines={5}
+                        textAlignVertical={'top'}
+                        containerStyle={{paddingHorizontal: 0}}
+                        inputContainerStyle={{
+                            paddingHorizontal: 5, borderRadius: 5,
+                            borderWidth: 1, borderColor: 'black'
+                        }}
+                        inputStyle={{color: "black"}}
+                        labelStyle={{
+                            fontSize: 20,
+                            color: "black",
+                            marginBottom: 5,
+                            fontWeight: "100"
+                        }}
+                        placeholderTextColor={COLORS.lightGrey}
+                        errorStyle={{color: "crimson"}}
+                    />
+
+                    <View className='flex-row justify-between'>
+
+
+                        <Button
+                            title="See preview"
+                            onPress={() => onShowPreview()}
+                            buttonStyle={{
+
+                                backgroundColor: COLORS.primary,
+                                padding: 10,
+                                borderRadius: 10,
+                            }}
+                            titleStyle={{fontSize: 20}}
+                            containerStyle={{
+                                flex: 1,
+                                marginVertical: 20,
+                                marginRight: 10,
+                            }}
+                        />
+
+                        <Button
+                            title="Post"
+                            onPress={() => onSendPost()}
+                            buttonStyle={{
+                                backgroundColor: "orange",
+                                padding: 10,
+                                borderRadius: 10,
+                            }}
+                            titleStyle={{fontSize: 20}}
+                            containerStyle={{
+                                flex: 1,
+                                marginVertical: 20,
+                                marginLeft: 10
+                            }}
+                        />
+                    </View>
+                </View>
+            </ScrollView>
+
+            <PostPreviewSheet
+                item={postData}
+                ref={previewSheetRef}
+            />
+        </>
     );
 };
 
