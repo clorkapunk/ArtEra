44282ce0690b518be91297a33124302613129482
clorkapunk <temir225531@gmail.com>
2024-05-05 22:52:44 +0500

    Changed user api

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,31 +1,31 @@
-import { $authHost, $host } from "./index";
-import { jwtDecode } from "jwt-decode";
+import {$authHost, $host} from "./index";
+import {jwtDecode} from "jwt-decode";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
-
 export const registration = async (email, password, username) => {
-  const { data } = await $host.post("api/users/", {
-    user: {
-      email, password, username,
-    },
-  })
-    .catch(e => console.log(`[${e.code}]`, e.message));
-  await AsyncStorage.setItem('user',JSON.stringify(data.user))
-  return jwtDecode(data.token)
+    const {data} = await $host.post("api/users/", {
+
+        email, password, username,
+
+    })
+        .catch(e => console.log(`[${e.code}]`, e.message));
+    await AsyncStorage.setItem('user', JSON.stringify(data))
+    return jwtDecode(data.token)
 };
 
 export const login = async (email, password) => {
-  const { data } = await $host.post("api/users/login/", {
-    user: {
-      email,
-      password
-    }
-  })
-  return data.user
+    const {data} = await $host.post("api/users/login/", {
+        email,
+        password
+    })
+
+    console.log(data)
+
+    return data
 };
 
 export const getUserData = async (userId) => {
-  const { data } = await $host.get("api/users/" + userId)
-  return data
+    const {data} = await $host.get("api/users/" + userId)
+    return data
 }
