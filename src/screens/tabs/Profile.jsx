3a7c62f14cd8d011d4a20d64f0a099f7e6e31603
clorkapunk <temir225531@gmail.com>
2024-05-05 22:54:14 +0500

    Reedited

--- src/screens/tabs/Profile.jsx
+++ src/screens/tabs/Profile.jsx
@@ -6,64 +6,95 @@ import {
   TouchableNativeFeedback,
   View,
   Image,
-  InteractionManager,
+  InteractionManager, RefreshControl, ToastAndroid,
 } from "react-native";
 import GridItem from "../../components/GridItem";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {getPostsBySearch} from "../../api/ContentAPI";
+import MasonryList from "@react-native-seoul/masonry-list";
+import {getUserData} from "../../api/userAPI";
 
 const Profile = () => {
-
-
-  const imgUrls = [
-    "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
-    "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-    "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg",
-    "https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340",
-  ];
-
-
-  function getPosts() {
-    let data = [];
-    for (let i = 0; i < 20; i++) {
-      data.push({
-        id: i,
-        user: `User ${i}`,
-        reactions: Math.round(Math.random() * 100),
-        comments: Math.round(Math.random() * 30),
-        img: imgUrls[Math.floor(Math.random() * imgUrls.length)],
-      });
-    }
-    return data;
-  }
-
-  const [tab, setTab] = useState("arts");
-
-  const [user, setUser] = useState({
-    nickname: "Die young",
-    profileImg: "https://unblast.com/wp-content/uploads/2021/01/Space-Background-Images.jpg",
-    followers: 500,
-    artsAmount: 31,
-    arts: [],
-    favorites: [],
-  });
-
-  useEffect(() => {
-    setUser({
-      nickname: "Die young",
-      followers: 500,
-      artsAmount: 31,
-      profileImg: "https://unblast.com/wp-content/uploads/2021/01/Space-Background-Images.jpg",
-      arts: getPosts(),
-      favorites: getPosts(),
-    });
-  }, []);
-
   const [isLoading, setIsLoading] = useState(true);
+  const [isSearchLoading, setIsSearchLoading] = useState(false)
+  const [tab, setTab] = useState("arts");
+  const [user, setUser] = useState({
+    id: '',
+    username: "",
+    user_background: "https://unblast.com/wp-content/uploads/2021/01/Space-Background-Images.jpg",
+    avatar: 'https://www.clevelanddentalhc.com/wp-content/uploads/2018/03/sample-avatar.jpg',
+    email: "",
+  });
+  const [data, setData] = useState({
+    count: null,
+    next: null,
+    previous: null,
+    results: []
+  });
+  const [refreshing, setRefreshing] = useState(false)
 
   useEffect(() => {
     InteractionManager.runAfterInteractions(() => {
       setIsLoading(false);
     });
   }, []);
+
+
+  useEffect(() => {
+    async function getUser(){
+      return JSON.parse(await AsyncStorage.getItem('user'))
+    }
+
+    getUser()
+        .then(user => {
+          getUserData(user.id).then(data => {
+            setUser(data)
+            console.log(`user is ${user.id}`)
+            getPostsBySearch('', data.id, 1)
+                .then(data => {
+                  setData(data)
+                  console.log(user)
+                })
+                .catch(e => console.log(e.response.data))
+          })
+              .catch(e => console.log(e.response.data))
+        })
+
+  }, []);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    getPostsBySearch('','', 1)
+        .then(data => {
+          setData(data);
+          setRefreshing(false);
+        })
+        .catch((e) => {
+          setRefreshing(false);
+        });
+  }, []);
+
+  const onEndReached = () => {
+    if (data.next === null) {
+      ToastAndroid.show("End reached!", ToastAndroid.SHORT)
+      return
+    }
+
+    getPostsBySearch('', '',data.next)
+        .then(data => {
+          setData(prevState => {
+            return {
+              next: data.next,
+              previous: data.previous,
+              count: data.count,
+              results: prevState.results.concat(data.results)
+            }
+          });
+        })
+        .catch((e) => {
+          console.log('error')
+        });
+  }
 
 
   return (
@@ -75,22 +106,35 @@ const Profile = () => {
           </View>
           :
           <View className="flex-1">
-            <Image
-              source={{ uri: user.profileImg }}
-              style={{ flex: 1 }}
-              resizeMode={"cover"}
-              PlaceholderContent={<ActivityIndicator />}
-            />
+            <View style={{flex: 1, position: 'relative'}}>
+              <Image
+                  source={{uri:  user.user_background}}
+                  style={{ flex: 1 }}
+                  resizeMode={"cover"}
+                  PlaceholderContent={<ActivityIndicator />}
+              />
+            </View>
+
             <View style={{ flex: 3 }} className="m-3">
-              <Text className="text-2xl font-cgbold mb-2">{user.nickname}</Text>
+              <View className='flex-row justify-between items-center mb-2'>
+                <Text className="text-2xl font-cgbold mb-2">{user.username}</Text>
+                <Image
+                    source={{uri:  user.avatar}}
+                    className="rounded-full"
+                    style={{ width: "28%", aspectRatio: 1 }}
+                    resizeMode={"cover"}
+                    PlaceholderContent={<ActivityIndicator />}
+                />
+              </View>
+
               <View className="flex-row mb-6">
                 <View className="mr-10">
                   <TouchableNativeFeedback
-                    onPress={() => alert("list of followers")}
+                    onPress={() => alert("Coming soon")}
                   >
                     <View className="p-1 rounded">
                       <Text className={`text-xl font-cgregular underline `}>
-                        {user.followers} followers
+                        0 followers
                       </Text>
                     </View>
                   </TouchableNativeFeedback>
@@ -100,7 +144,7 @@ const Profile = () => {
                 >
                   <View className="p-1 rounded">
                     <Text className={`text-xl font-cgregular underline `}>
-                      {user.artsAmount} arts
+                      {data.count} arts
                     </Text>
                   </View>
                 </TouchableNativeFeedback>
@@ -124,15 +168,28 @@ const Profile = () => {
               </View>
 
               <View style={{ flex: 1 }}>
-                <FlatList
-                  columnWrapperStyle={{
-                    justifyContent: "space-between",
-                  }}
-                  numColumns={2}
-                  data={user[tab]}
-                  renderItem={({ item }) => (<GridItem item={item} />)}
-                  keyExtractor={(item, index) => item.id}
-                />
+                {
+                  isSearchLoading ?
+                      <ActivityIndicator size={30} className='mb-2'/>
+                      :
+                      <MasonryList
+                          refreshControl={
+                            <RefreshControl
+                                enabled={true}
+                                refreshing={refreshing}
+                                onRefresh={onRefresh}
+                            />
+                          }
+                          onEndReached={() => onEndReached()}
+                          columnWrapperStyle={{
+                            justifyContent: "space-between",
+                          }}
+                          numColumns={2}
+                          data={data.results}
+                          renderItem={({item}) => (<GridItem item={item}/>)}
+                          keyExtractor={(item, index) => item.id}
+                      />
+                }
               </View>
             </View>
 
