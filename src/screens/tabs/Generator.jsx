190646ed5dce86bd9adaa61372898a3da5a7205e
clorkapunk <temir225531@gmail.com>
2024-05-09 16:02:40 +0500

    Created

---
+++ src/screens/tabs/Generator.jsx
@@ -0,0 +1,25 @@
+import React, { memo, useEffect, useState } from "react";
+import { Button, ScrollView, Text, TouchableNativeFeedback, View, Image } from "react-native";
+
+
+
+const Generator = () => {
+
+
+
+  useEffect(() => {
+
+  }, [])
+
+
+
+
+
+  return (
+    <View className='flex-1 h-full w-full justify-center items-center'>
+      <Text className='text-2xl'>COMING SOON</Text>
+    </View>
+  );
+}
+
+export default memo(Generator);
