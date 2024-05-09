86d20b936025e4a46fe8dd8bcd6367428111d16b
clorkapunk <temir225531@gmail.com>
2024-05-09 16:01:05 +0500

    Added animations

--- src/screens/auth/AuthLayout.jsx
+++ src/screens/auth/AuthLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import Header from "../../components/Header";
@@ -7,22 +7,31 @@ import Header from "../../components/Header";
 const Stack = createNativeStackNavigator()
 
 const AuthLayout = () => {
-  return (
-    <>
-      <Stack.Navigator
-        initialRouteName={'sign-in'}
-        screenOptions={{
-          headerShown: false
-        }}
-      >
-        <Stack.Screen
-          name='sign-in'
-          component={SignIn}
-        />
-        <Stack.Screen name='sign-up' component={SignUp}/>
-      </Stack.Navigator>
-    </>
-  );
+    return (
+        <>
+            <Stack.Navigator
+                initialRouteName={'sign-in'}
+                screenOptions={{
+                    headerShown: false
+                }}
+            >
+                <Stack.Screen
+                    name='sign-in'
+                    component={SignIn}
+                    options={{
+                        animation: 'slide_from_left'
+                    }}
+                />
+                <Stack.Screen
+                    name='sign-up'
+                    component={SignUp}
+                    options={{
+                        animation: 'slide_from_left'
+                    }}
+                />
+            </Stack.Navigator>
+        </>
+    );
 };
 
 export default AuthLayout;
