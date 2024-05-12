2d0ede630ccf8a72bfa246a45e2a8099a588755b
clorkapunk <temir225531@gmail.com>
2024-05-12 12:18:00 +0500

    Commit

---
+++ src/components/StyledFontAwesomeIcon.js
@@ -0,0 +1,6 @@
+import {styled} from "nativewind";
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)
+
+export default StyledFontAwesomeIcon
