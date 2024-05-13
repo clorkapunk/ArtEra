68936943a3f917122dfe0a386f3a7e80cc76f27d
clorkapunk <temir225531@gmail.com>
2024-05-13 15:04:41 +0500

    Added new colors

--- src/consts/colors.js
+++ src/consts/colors.js
@@ -35,5 +35,48 @@ export const colors = {
       border: '#0F0802',
       icon: '#0F0802'
     }
+  },
+  search: {
+    icon: '#0F0802',
+    clear: '#0F0802',
+    placeholder: '#666666',
+    text: '#0F0802',
+    border: '#0F0802'
+  },
+  generator: {
+    image: {
+      text: '#0F0802',
+      border: '#0F0802'
+    },
+    progress: {
+      text: '#0F0802',
+      line: {
+        before: '#bd782d',
+        after: '#666666'
+      }
+    },
+    input: {
+      placeholder: '#666666',
+      border: '#0F0802',
+      text: '#0F0802'
+    },
+    slider: {
+      text: '#0F0802',
+      before: '#0F0802',
+      after: '#666666',
+      thumb: '#bd782d'
+    },
+    buttons: {
+      generate: {
+        border: '#0F0802',
+        bg: '#ff8077',
+        text:  '#0F0802'
+      },
+      create: {
+        border: '#0F0802',
+        bg: '#f9bb05',
+        text: '#0F0802'
+      }
+    }
   }
 }
