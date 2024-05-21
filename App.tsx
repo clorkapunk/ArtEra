82f700bd53f7a6578237490bddddd10468db7135
clorkapunk <temir225531@gmail.com>
2024-05-21 17:02:39 +0500

    Added theme context

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-import React, {useEffect} from "react";
+import React, {createContext, useContext, useEffect} from "react";
 import {DefaultTheme, NavigationContainer} from "@react-navigation/native";
 import TabsLayout from "./src/screens/tabs/TabsLayout";
 import AuthLayout from "./src/screens/auth/AuthLayout";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
-import {PermissionsAndroid, Platform} from "react-native";
+import {Appearance, PermissionsAndroid, Platform} from "react-native";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import PostScreen from "./src/screens/PostScreen";
 import {createStackNavigator, TransitionPreset, TransitionPresets} from "@react-navigation/stack";
 import ProfileEdit from "./src/screens/ProfileEdit";
 import "./global.css"
 import ProfileUser from "./src/screens/ProfileUser";
-import {colors} from './src/consts/colors'
+import ThemeContext from "./src/context/ThemeProvider";
 
 
 const Stack = createStackNavigator();
@@ -51,72 +51,78 @@ async function hasAndroidPermission() {
     return await getRequestPermissionPromise();
 }
 
-function App(): React.JSX.Element {
 
+
+function App(): React.JSX.Element {
 
 
     useEffect(() => {
         hasAndroidPermission().then();
     }, [])
 
-    return (
-        <SafeAreaProvider>
-            <GestureHandlerRootView>
-                <NavigationContainer
-                    theme={{
-                        ...DefaultTheme,
-                        colors: {
-                            ...DefaultTheme.colors,
-                            background: colors.background
-                        }
-                    }}
-                >
-                    <Stack.Navigator
-                        screenOptions={{
-                            headerShown: false,
-                        }}
-                        initialRouteName={"tabs"}
-                    >
-                        <Stack.Screen
-                            name="tabs"
-                            component={TabsLayout}
-                            options={{
-                                ...TransitionPresets.SlideFromRightIOS
-                            }}
-                        />
-                        <Stack.Screen
-                            name="auth"
-                            component={AuthLayout}
-                            options={{
-                                ...TransitionPresets.SlideFromRightIOS
-                            }}
-                        />
-                        <Stack.Screen
-                            name="post-screen"
-                            component={PostScreen}
-                            options={{
-                                ...TransitionPresets.SlideFromRightIOS
-                            }}
-                        />
-                        <Stack.Screen
-                            name="profile-edit"
-                            component={ProfileEdit}
-                            options={{
-                                ...TransitionPresets.SlideFromRightIOS
-                            }}
-                        />
-                        <Stack.Screen
-                            name="profile-user"
-                            component={ProfileUser}
-                            options={{
-                                ...TransitionPresets.SlideFromRightIOS
-                            }}
-                        />
-                    </Stack.Navigator>
+    let {colors} = useContext(ThemeContext)
 
-                </NavigationContainer>
-            </GestureHandlerRootView>
-        </SafeAreaProvider>
+
+
+    return (
+
+            <SafeAreaProvider>
+                <GestureHandlerRootView>
+                    <NavigationContainer
+                        theme={{
+                            ...DefaultTheme,
+                            colors: {
+                                ...DefaultTheme.colors,
+                                background: colors.background
+                            }
+                        }}
+                    >
+                        <Stack.Navigator
+                            screenOptions={{
+                                headerShown: false,
+                            }}
+                            initialRouteName={"tabs"}
+                        >
+                            <Stack.Screen
+                                name="tabs"
+                                component={TabsLayout}
+                                options={{
+                                    ...TransitionPresets.SlideFromRightIOS
+                                }}
+                            />
+                            <Stack.Screen
+                                name="auth"
+                                component={AuthLayout}
+                                options={{
+                                    ...TransitionPresets.SlideFromRightIOS
+                                }}
+                            />
+                            <Stack.Screen
+                                name="post-screen"
+                                component={PostScreen}
+                                options={{
+                                    ...TransitionPresets.SlideFromRightIOS
+                                }}
+                            />
+                            <Stack.Screen
+                                name="profile-edit"
+                                component={ProfileEdit}
+                                options={{
+                                    ...TransitionPresets.SlideFromRightIOS
+                                }}
+                            />
+                            <Stack.Screen
+                                name="profile-user"
+                                component={ProfileUser}
+                                options={{
+                                    ...TransitionPresets.SlideFromRightIOS
+                                }}
+                            />
+                        </Stack.Navigator>
+
+                    </NavigationContainer>
+                </GestureHandlerRootView>
+            </SafeAreaProvider>
     );
 }
 
--- src/consts/colors.js
+++ src/consts/colors.js
@@ -1,88 +1,181 @@
+
+
 export const colors = {
-  background: "#FAF9EF",
-  header: {
-    text: '#0F0802',
-    menu: '#0F0802',
-    bg: "#D9D9D9",
-    border: "#0F0802",
-  },
-  tabbar: {
-    bg: "#132B37",
-    tab: {
-      active: {
-        icon: "#BD782D",
-        text: "#FAF9EF",
-      },
-      inactive: "#BD782D",
-      center: {
-        icon: '#FAF9EF',
-        text: "#BD782D"
-      }
-    },
-    border: "#FAF9EF",
-  },
-  listitem: {
-    username: '#0F0802',
-    title: '#0F0802',
-    description: '#0F0802',
-    like: {
-      active: '#bd782d',
-      inactive: '#0F0802',
-      text: '#0F0802'
-    },
-    comment: {
-      icon: '#0F0802',
-      text: '#0F0802'
-    },
-    input: {
-      bg: 'transparent',
-      placeholder: '#666666',
-      text: '#0F0802',
-      border: '#0F0802',
-      icon: '#0F0802'
-    }
-  },
-  search: {
-    icon: '#0F0802',
-    clear: '#0F0802',
+  light: {
+    header: "#D9D9D9",
+    background: "#FAF9EF",
+    errorRed: '#ff0000',
+    primary: "#BD782D",
+    secondary: "#132B37",
     placeholder: '#666666',
-    text: '#0F0802',
-    border: '#0F0802'
-  },
-  generator: {
-    image: {
-      text: '#0F0802',
-      border: '#0F0802'
-    },
-    progress: {
-      text: '#0F0802',
-      line: {
-        before: '#bd782d',
-        after: '#666666'
-      }
-    },
-    input: {
-      placeholder: '#666666',
-      border: '#0F0802',
-      text: '#0F0802'
-    },
-    slider: {
-      text: '#0F0802',
-      before: '#0F0802',
-      after: '#666666',
-      thumb: '#bd782d'
-    },
+    tertiary: "#FAF9EF",
     buttons: {
-      generate: {
-        border: '#0F0802',
-        bg: '#ff8077',
-        text:  '#0F0802'
-      },
-      create: {
-        border: '#0F0802',
-        bg: '#f9bb05',
-        text: '#0F0802'
-      }
-    }
-  }
+      generate: '#ff8077',
+      create: '#f9bb05',
+      post: '#0F0802',
+      preview: "#BD782D",
+      confirm: '#f9bb05',
+      image_edit: '#ff8077',
+      signin: '#f9bb05',
+      signup: '#ff8077'
+    },
+    main: '#0F0802',
+    main_contrast: "#FAF9EF"
+  },
+  dark: {
+    header: "#15141B",
+    background: "#15141B",
+    errorRed: '#ff0000',
+    primary: "#00919B",
+    secondary: "#070F13",
+    tertiary: "#742F8C",
+    placeholder: '#666666',
+    buttons: {
+      generate: '#ff8077',
+      create: '#f9bb05',
+      post: '#00919B',
+      preview: "#0644FA",
+      confirm: '#f9bb05',
+      image_edit: '#ff8077',
+      signin: '#f9bb05',
+      signup: '#ff8077'
+    },
+    main: "#FAF9EF",
+    main_contrast:"#FAF9EF"
+  },
 }
+
+
+// export const colors = {
+//   background: "#FAF9EF",
+//   errorRed: '#ff0000',
+//
+//   header: {
+//     text: '#0F0802',
+//     menu: '#0F0802',
+//     bg: "#D9D9D9",
+//     border: "#0F0802",
+//   },
+//   tabbar: {
+//     bg: "#132B37",
+//     tab: {
+//       active: {
+//         icon: "#BD782D",
+//         text: "#FAF9EF",
+//       },
+//       inactive: "#BD782D",
+//       center: {
+//         icon: '#FAF9EF',
+//         text: "#BD782D"
+//       }
+//     },
+//     border: "#FAF9EF",
+//   },
+//   listitem: {
+//     username: '#0F0802',
+//     title: '#0F0802',
+//     description: '#0F0802',
+//     like: {
+//       active: '#bd782d',
+//       inactive: '#0F0802',
+//       text: '#0F0802'
+//     },
+//     comment: {
+//       icon: '#0F0802',
+//       text: '#0F0802'
+//     },
+//     input: {
+//       bg: 'transparent',
+//       placeholder: '#666666',
+//       text: '#0F0802',
+//       border: '#0F0802',
+//       icon: '#0F0802'
+//     }
+//   },
+//   search: {
+//     icon: '#0F0802',
+//     clear: '#0F0802',
+//     placeholder: '#666666',
+//     text: '#0F0802',
+//     border: '#0F0802'
+//   },
+//   generator: {
+//     image: {
+//       text: '#0F0802',
+//       border: '#0F0802'
+//     },
+//     progress: {
+//       text: '#0F0802',
+//       line: {
+//         before: '#bd782d',
+//         after: '#666666'
+//       }
+//     },
+//     input: {
+//       placeholder: '#666666',
+//       border: '#0F0802',
+//       text: '#0F0802'
+//     },
+//     slider: {
+//       text: '#0F0802',
+//       before: '#0F0802',
+//       after: '#666666',
+//       thumb: '#bd782d'
+//     },
+//     buttons: {
+//       generate: {
+//         border: '#0F0802',
+//         bg: '#ff8077',
+//         text:  '#0F0802'
+//       },
+//       create: {
+//         border: '#0F0802',
+//         bg: '#f9bb05',
+//         text: '#0F0802'
+//       }
+//     }
+//   },
+//   profile: {
+//     username: '#0F0802',
+//     info_labels: '#0F0802',
+//     tabs: {
+//       text: '#0F0802',
+//       bg: "#FAF9EF",
+//     }
+//   },
+//   sidebar: {
+//     bg: "#132B37",
+//     text: "#FAF9EF",
+//     icon: "#FAF9EF",
+//     arrow: "#FAF9EF"
+//   },
+//   post: {
+//     labels: '#0F0802',
+//     selected: {
+//       text: "#FAF9EF",
+//       bg:"#BD782D",
+//     },
+//     button: {
+//       bg: '#0F0802',
+//       text: "#FAF9EF"
+//     }
+//   },
+//   post_create:{
+//     input: {
+//       text: '#0F0802',
+//       placeholder: '#666666',
+//       underline: '#0F0802'
+//     },
+//     preview_btn: {
+//       bg: '',
+//       text: ''
+//     },
+//     post_btn: {
+//       bg: '',
+//       text: ''
+//     }
+//   }
+// }
+
+
+
